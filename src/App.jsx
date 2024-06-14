23a84ff29e0baa47f768e2eaeb1d1ba976c87de1
FernuDev <fernu292@gmail.com>
2024-06-13 21:57:58 -0600

    Fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import './styles/App.css';
 import React from 'react';
 import Header from './components/Header';
-import { useContext } from 'react';
-import { calculatorContext } from './Context/Calculator_context';
 
 const App = () => {
 
-  const func = (x)=> {
-    return x*x
-  }
-
-  const {round} = useContext(calculatorContext);
-
-  console.log(round(1.5123253251, -4));
   return (
     <div>
       <Header />
